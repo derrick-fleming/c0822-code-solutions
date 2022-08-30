fae2078017c8d618e05d57c53fdb19cf58273fdf
Derrick Fleming <derrick.fleming7@gmail.com>
2022-08-30 17:09:39 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,67 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveredDate: 'Aug 8, 2020',
+    itemsOrdered: [
+      {
+        itemImage: ' ',
+        item: ['JavaScript for impatient programmers', 'book', 'Rauschmayer, Dr. Axel'],
+        itemPrice: '$31.55'
+      }],
+    returnInfo: 'Return window closed on Sep 7, 2020'
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveredDate: 'Jul 20, 2020',
+    itemsOrdered: [
+      {
+        itemImage: ' ',
+        item: ['The Timeless Way of Building', 'book', 'Alexander, Christopher'],
+        itemPrice: '$41.33'
+      }],
+    returnInfo: 'Return window closed on Aug 19, 2020'
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-287557-9059409',
+    deliveredDate: 'Jul 7, 2020',
+    itemOrdered: [
+      {
+        itemImage: ' ',
+        item: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
+          'hardware'],
+        itemPrice: '$15.98'
+      }],
+    returnInfo: 'Return window closed on Aug 5, 2020'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveredDate: 'Jul 5, 2020',
+    itemOrdered: [
+      {
+        itemImage: ' ',
+        item: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+          'hardware'],
+        itemPrice: '$94.95'
+      },
+      {
+        itemImage: ' ',
+        item: ['The Art of Sql', 'book', 'Faroult, Stephanie'],
+        itemPrice: '$33.99'
+      }],
+    returnInfo: 'Return window closed on Aug 4, 2020'
+  }
+];
+
+console.log(orderHistory);
