187ba067654f98a065d2e2b7e7099c6624a895e7
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-02 03:45:18 +0000

    saved get-words and reverse-words functions.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -3,8 +3,8 @@
 /* 1. Create a storage of an empty container for the array
 2. If the string has a value inside of it...
 3. Split the string into an array using the split method.
-4. If it does NOT have an array inside of it, set container to an empty array
-Return the value of the array */
+4. If it does NOT have an array inside of it, return empty array
+5. Return the value of the array */
 
 function getWords(string) {
   var getWordsOutput = [];
