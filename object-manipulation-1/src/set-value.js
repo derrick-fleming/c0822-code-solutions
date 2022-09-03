858610fdc18cfbf4875e7aec340dde3e74c86669
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-03 16:50:19 +0000

    Saved set-value function.

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,6 +1,6 @@
 /* exported setValue */
 
-/* 1. Set the functiont to take three parameters: object, key, value
+/* 1. Define the functiont to take three parameters: object, key, value
 2. Add the key & value parameter to the original object by:
      - Assigning the value to the object's key using bracket notation
 3.  Return nothing. */
