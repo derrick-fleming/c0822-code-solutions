5cca285cfb963cada6635f5ab241648a4e0dff72
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-03 16:07:23 +0000

    Fixed getWords function to not include extra return statement in the conditional.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -2,15 +2,14 @@
 
 /* 1. Create a storage of an empty container for the array
 2. If the string has a value inside of it...
-3. Split the string into an array using the split method.
-4. If it does NOT have an array inside of it, return empty array
+3. Split the string into an array using the split method & store value into output.
+4. If it does NOT have an array inside of it, don't do anything.
 5. Return the value of the array */
 
 function getWords(string) {
   var getWordsOutput = [];
   if (string !== '') {
     getWordsOutput = string.split(' ');
-    return getWordsOutput;
   }
   return getWordsOutput;
 }
