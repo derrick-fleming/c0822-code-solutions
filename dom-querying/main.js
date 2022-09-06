7d0232721d2c74235a09f78f523242aeb4599418
Derrick Fleming <derrick.fleming7@gmail.com>
2022-09-06 16:45:25 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,12 @@
+console.log('hello, world');
+console.log('h1 DOM query:', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+
+console.log('ID Selector explanation:', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+console.log('DOM query for hint class:', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+
+console.log('DOM query for all p elements:', document.querySelectorAll('p'));
+console.log('DOM query for all class elements of example-link:', document.querySelectorAll('.example-link'));
