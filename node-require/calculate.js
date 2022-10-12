eb3838e1ae59ca24484b5ef9f48a6e93eb8c757c
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-12 03:46:08 +0000

    Used 'striclty equals to' instead of 'includes' in conditional statement with process.agrv[3].

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -6,22 +6,22 @@ const divide = require('./divide');
 const num1 = Number(process.argv[2]);
 const num2 = Number(process.argv[4]);
 
-if (process.argv[3].includes('plus')) {
+if (process.argv[3] === 'plus') {
   const addResult = add(num1, num2);
   console.log('Result:', addResult);
 }
 
-if (process.argv[3].includes('minus')) {
+if (process.argv[3] === 'minus') {
   const subtractResult = subtract(num1, num2);
   console.log('Result:', subtractResult);
 }
 
-if (process.argv[3].includes('over')) {
+if (process.argv[3] === 'over') {
   const divideResult = divide(num1, num2);
   console.log('Result:', divideResult);
 }
 
-if (process.argv[3].includes('times')) {
+if (process.argv[3] === 'times') {
   const multiplyResult = multiply(num1, num2);
   console.log('Reuslt:', multiplyResult);
 }
