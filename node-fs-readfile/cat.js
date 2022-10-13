656e6910ba4c30de600c76a5d08f8f45ae84fbf6
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-13 16:24:25 +0000

    Updated cat.js to recursively call the catFile function.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
-let dataStorage = '';
-let boolean = false;
 
-for (let i = 2; i < process.argv.length; i++) {
-  const fileName = process.argv[i];
-  fs.readFile(fileName, 'utf8', (err, data) => {
-    if (err) throw err;
-    dataStorage = dataStorage + data + '\n';
-    if (i === process.argv.length - 1) {
-      boolean = true;
+catFile(2);
+function catFile(i) {
+  if (i === process.argv.length) {
+    return;
+  }
+  fs.readFile(process.argv[i], 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
     }
-    if (boolean === true) {
-      console.log(dataStorage);
-    }
+    console.log(data);
+    catFile(i + 1);
   });
 }
