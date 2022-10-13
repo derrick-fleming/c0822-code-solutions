7907670b54f1dcbbfe33962aca8fc765561e641f
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-13 16:52:28 +0000

    Fixed cp.js to copy one file to another file.

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -1,17 +1,14 @@
 const fs = require('fs');
-let dataOutput = '';
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  dataOutput = dataOutput + data + '\n';
-});
-
-fs.writeFile(process.argv[3], dataOutput, 'utf8', err => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+  fs.writeFile(process.argv[3], data, 'utf8', err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
 });
