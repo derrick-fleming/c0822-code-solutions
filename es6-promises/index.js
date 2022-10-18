5521f8af314f0f07779d7aa0b3bf9b0974c4a454
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-18 23:18:55 +0000

    Updated exercise to include the catch() method in solution.

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -3,6 +3,7 @@ const takeAChance = require('./take-a-chance');
 const promiseReturn = takeAChance('Derrick');
 promiseReturn.then(value => {
   console.log(value);
-}, reason => {
-  console.log(reason.message);
+});
+promiseReturn.catch(error => {
+  console.log(error.message);
 });
