0c6c875848cc1c5fbfb9f4424723112ee8695a6b
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-20 22:04:23 +0000

    Revised reduce function to remove unnecessary conditional of i strictly equal to 0.

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -5,30 +5,8 @@ function reduce(array, reducer, initialValue) {
       initialValue = array[0];
       output = initialValue;
       i = 1;
-    } else if (i === 0) {
-      output = initialValue;
     }
     output = reducer(output, array[i]);
   }
   return output;
-}
-
-function sumFunction(output, value) {
-  return output + value;
-}
-
-function productFunction(previousValue, currentValue) {
-  return previousValue * currentValue;
-}
-
-function balanceFunction(previousValue, currentValue) {
-  if (currentValue.type === 'deposit') {
-    return previousValue + currentValue.amount;
-  } else {
-    return previousValue - currentValue.amount;
-  }
-}
-
-function compositeFunction(previousValue, currentValue) {
-  return Object.assign(previousValue, currentValue);
 }
