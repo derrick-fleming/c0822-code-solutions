15d51d341cc0a2b2a0ab7d3fd5be020c4cbaba80
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-20 21:59:17 +0000

    Added a conditional statement to allow initialValue to be optional

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -1,7 +1,34 @@
 function reduce(array, reducer, initialValue) {
-  let output = initialValue;
+  let output;
   for (let i = 0; i < array.length; i++) {
+    if (initialValue === undefined) {
+      initialValue = array[0];
+      output = initialValue;
+      i = 1;
+    } else if (i === 0) {
+      output = initialValue;
+    }
     output = reducer(output, array[i]);
   }
   return output;
+}
+
+function sumFunction(output, value) {
+  return output + value;
+}
+
+function productFunction(previousValue, currentValue) {
+  return previousValue * currentValue;
+}
+
+function balanceFunction(previousValue, currentValue) {
+  if (currentValue.type === 'deposit') {
+    return previousValue + currentValue.amount;
+  } else {
+    return previousValue - currentValue.amount;
+  }
+}
+
+function compositeFunction(previousValue, currentValue) {
+  return Object.assign(previousValue, currentValue);
 }
