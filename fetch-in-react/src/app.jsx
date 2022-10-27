744aca3e31fc125f9b2279fcfa4293553e631089
Derrick Fleming <derrick.fleming7@gmail.com>
2022-10-27 22:32:35 +0000

    Completed fetch-in-react exercise.

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(usersArray => {
+        this.setState({
+          users: usersArray,
+          isLoading: false
+        });
+      })
+      .catch(err => console.error('There was an error!', err));
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
