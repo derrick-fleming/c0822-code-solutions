082a80aafa0a27d51599950f46d4a100dc8ab75c
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-05 21:26:31 +0000

    Updated hot-button-component exercise to include react hooks that useState instead of this.setState.

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CustomButton from './hot-button';
+import CustomButtonHook from './hook-hot-button';
 
 const element = (
+  <>
   <CustomButton />
+  <CustomButtonHook />
+  </>
 );
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
