0bbd3c8d3fc5edaf7fcc2625f7e441c3a8b2c8b6
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-07 01:01:06 +0000

    Created a timer using the useEffect() and reset the timer using a resetTimer() function to reset the count to 0.

--- react-hook-stopwatch-component/src/hook-stopwatch.jsx
+++ react-hook-stopwatch-component/src/hook-stopwatch.jsx
@@ -4,35 +4,41 @@ const Stopwatch = () => {
   const [count, setCount] = useState(0);
   const [play, setPlay] = useState(false);
 
-  useEffect(() => {
-    let timer;
-    if (!play) {
-      clearInterval(timer);
+  function toggleStart() {
+    if (play !== false) {
+      setPlay(false);
     } else {
-      timer = setInterval(() => {
-        setCount(count + 1);
-      }, 1000);
+      setPlay(true);
     }
+  }
 
-  });
+  useEffect(() => {
+    let interval = null;
+    if (play) {
+      interval = setInterval(() => setCount(prevCount => prevCount + 1), 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [play]);
+
+  function resetTimer() {
+    if (!play) {
+      setCount(0);
+    }
+  }
 
   const iconClass = play
-    ? 'play'
-    : 'pause';
+    ? 'pause'
+    : 'play';
 
   return (
     <div className='container'>
-      <div className='watch-face'>
+      <div className='watch-face' onClick={resetTimer}>
         <h1>{count}</h1>
       </div>
       <div className='container'>
-        <button onClick={() => {
-          if (!play) {
-            setPlay(true);
-          } else {
-            setPlay(false);
-          }
-        }}
+        <button onClick={toggleStart}
           className='playButton'>
           <i className={`fa-solid fa-${iconClass}`}></i>
         </button>
